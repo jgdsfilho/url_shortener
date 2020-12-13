0755f78ca9866d030cbcffc87be5fe264c915d11
Genilson <genilson974@gmail.com>
2020-12-13 19:12:30 -0300

    Adding more tests from url api

--- core/tests/test_url.py
+++ core/tests/test_url.py
@@ -3,9 +3,10 @@ from django.test.client import Client
 from django.test.testcases import TestCase
 from core.tests import fixtures
 import json
+from core.models import Url
 
 
-class TestAuthApi(TestCase):
+class TestUrlApi(TestCase):
     @classmethod
     def setUpTestData(cls):
         fixtures.user_jon()
@@ -13,13 +14,32 @@ class TestAuthApi(TestCase):
 
     def test_list_url_from_a_user(self):
         client = Client()
-        client.force_login(User.objects.get(username='jon'))
-        r1 = client.get('/api/url')
+        client.force_login(User.objects.get(username="jon"))
+        r1 = client.get("/api/url")
         self.assertEqual({200}, {r.status_code for r in [r1]})
 
     def test_creat_new_short_url(self):
         client = Client()
-        client.force_login(User.objects.get(username='jon'))
-        r1 = client.post('/api/url', {'url': 'https://www.buser.com.br/'})
+        client.force_login(User.objects.get(username="jon"))
+        r1 = client.post("/api/url", {"url": "https://www.buser.com.br/"})
         self.assertEqual(201, r1.status_code)
 
+    def test_creat_new_short_url_without_url(self):
+        client = Client()
+        client.force_login(User.objects.get(username="jon"))
+        r1 = client.post("/api/url")
+        self.assertEqual(400, r1.status_code)
+
+    def test_get_url(self):
+        client = Client()
+        client.force_login(User.objects.get(username="jon"))
+        url = Url.objects.all().first()
+        r1 = client.get(f"/api/redirect_url/{url.short_url}")
+        self.assertEqual(200, r1.status_code)
+
+    def test_get_url_not_found(self):
+        client = Client()
+        client.force_login(User.objects.get(username="jon"))
+        url = Url.objects.all().first()
+        r1 = client.get(f'/api/redirect_url/"54rfew"')
+        self.assertEqual(404, r1.status_code)
