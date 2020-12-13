d1a68f10f0aaf0c9358aa5c80f5bee1d6e228ead
Genilson <genilson974@gmail.com>
2020-12-13 20:48:31 -0300

    Create page to url

--- frontend/components/url-redirect.vue
+++ frontend/components/url-redirect.vue
@@ -1,5 +1,5 @@
 <template>
-  <div/>
+  <div />
 </template>
 
 <script>
@@ -8,38 +8,16 @@ import api from '~api'
 export default {
   data () {
     return {
-      url: '',
-      adding: false,
-      loading: false,
-      items: [
-      ]
+
     }
   },
   async mounted () {
-    this.loading = true
-    const response = await api.list_urls()
-    const urls = response.urls
-    this.items = urls
-    console.log(this.$route)
-    window.location.replace('https://google.com')
-    this.loading = false
-  },
-  methods: {
-    async add_url () {
-      this.adding = true
-      const response = await api.new_url(this.url)
-      this.items.push(response)
-      this.url = ''
-      this.adding = false
+    const response = await api.redirect_url(this.$route.params.url)
+    let url = response.url
+    if (!response.url.includes('http')) {
+      url = `http://${response.url}`
     }
+    window.location.href = url
   }
 }
 </script>
-
-{/* <a :href="myUrl">Minha URL</a>
-const response = await myRequest(...)
-const url = await response.json()
-
-window.location.href = url
-Felipe Gomes de Oliveira11:20 AM
-window.location.replace(url) */}
---
+++ frontend/pages/urls_list.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout justify-left>
+    <url-list />
+  </v-layout>
+</template>
+
+<script>
+import urlList from '~/components/url-list.vue'
+
+export default {
+  layout: 'complex',
+  components: {
+    urlList
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
