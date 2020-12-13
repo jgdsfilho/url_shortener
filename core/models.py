59f72c939ab01758346713bb0aff385d08fd3c54
Genilson <genilson974@gmail.com>
2020-12-13 19:12:14 -0300

    Creating test to url services

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class DateBaseModel(models.Model):
     class Meta:
         abstract = True
@@ -12,24 +13,39 @@ class DateBaseModel(models.Model):
 class Url(DateBaseModel):
     url = models.CharField(max_length=255)
     short_url = models.CharField(max_length=255)
-    logged_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    logged_user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    def to_dict_json(self):
+        return {
+            "id": self.id,
+            "url": self.url,
+            "short_url": self.short_url,
+            "user": self.logged_user.email,
+        }
+
 
 class ActivityLog(models.Model):
     type = models.CharField(max_length=64)
-    logged_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
-    fromuser = models.ForeignKey(User, null=True, blank=True, related_name="activitylogs_withfromuser", on_delete=models.SET_NULL)
+    logged_user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL
+    )
+    fromuser = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        related_name="activitylogs_withfromuser",
+        on_delete=models.SET_NULL,
+    )
     jsondata = models.TextField(null=True, blank=True)
-    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    created_at = models.DateTimeField("criado em", auto_now_add=True)
 
     class Meta:
-        ordering = ('-created_at',)
+        ordering = ("-created_at",)
 
     def __str__(self):
-        return '%s / %s / %s' % (
-            self.type,
-            self.logged_user,
-            self.created_at,
-        )
+        return "%s / %s / %s" % (self.type, self.logged_user, self.created_at,)
 
 
 # class Todo(models.Model):
---
+++ core/tests/test_service_url.py
@@ -0,0 +1,37 @@
+from core.models import User, Url
+from django.test.testcases import TestCase
+from core.tests import fixtures
+from core.service.url_service import list_urls, add_url, get_url
+import json
+
+
+class TestServiceUrl(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        fixtures.user_jon()
+        fixtures.short_url()
+
+    def test_get_url_from_a_user(self):
+        user = User.objects.all().first()
+        urls_expected = [
+            url.to_dict_json() for url in Url.objects.filter(logged_user=user)
+        ]
+        urls = list_urls(user)
+        self.assertEqual(urls_expected, urls)
+
+    def test_create_url_to_a_user(self):
+        user = User.objects.all().first()
+        url = add_url("buser.com.br", user.id)
+        url_expected = Url.objects.get(
+            logged_user=user, url="buser.com.br"
+        ).to_dict_json()
+        self.assertEqual(url_expected, url)
+
+    def test_get_specific_url(self):
+        expected_url = Url.objects.all().first().to_dict_json()
+        url = get_url(expected_url["short_url"])
+        self.assertEqual(expected_url, url)
+
+    def test_get_specific_url_not_found(self):
+        url = get_url("543trew")
+        self.assertEqual(False, url)
